f85d125067973eef0d725dd3d1e94ae15679107a
Ellika LeSage <Elesage1@hwemail.com>
2025-09-16 21:46:22 -0700

    Adds file stringification functionality
    Implements a method to read and return the contents of a file as a string.
    This allows for easier processing and manipulation of file data.

--- MyFileWriterTester.java
+++ MyFileWriterTester.java
@@ -1,6 +1,15 @@
+import java.io.IOException;
+
 public class MyFileWriterTester {
     public static void main(String[] args) {
         //MyFileWriter.generateHiddenFile("hi", ".myFileFun");
         //MyFileWriter.generateRegularFile();
+        try {
+            String answer = MyFileWriter.stringify("regularFile");
+            System.out.println(answer);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
